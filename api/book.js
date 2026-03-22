903a1b946af37b97d551eb39c2e7a8006d0d8f2a
Ace <lowsetmedia@gmail.com>
2026-03-21 21:36:11 -0400

    testing vercel api

---
+++ api/book.js
@@ -0,0 +1,11 @@
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  const data = req.body;
+
+  console.log("Booking received:", data);
+
+  return res.status(200).json({ success: true });
+}
